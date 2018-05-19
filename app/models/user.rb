71be367f3ceea47137abeab60af5e73b5acf94bc
DanielRusinov <daniel.rusinov00@gmail.com>
2018-05-20 01:04:54 +0300

    added validations for the User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,13 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable, :confirmable, 
          :recoverable, :rememberable, :trackable, :validatable
+         
+  validates :first_name, :last_name, :country, :city, presence: true
+  validates :username, presence: true, uniqueness: true
+  validates :phone_number, presence: true,
+       numericality: { :message => ": Only positive number without spaces are allowed" },
+       uniqueness: true, length: { :minimum => 10, :maximum => 15 }
+  validates :email, presence: true, uniqueness: true,
+      format: { with: /\A[^@\s]+@([^@.\s]+.)+[^@.\s]+\z/ ,
+      :message => ": Please enter a valid email" }
 end
