6638a33ca6c686694a471447deacd291ac7f6cdd
ivanviyachki <il.viyachki@gmail.com>
2018-05-24 18:29:44 +0300

    Design Update

--- db/migrate/20180524133649_remove_avatar_from_listings.rb
+++
@@ -1,8 +0,0 @@
-class RemoveAvatarFromListings < ActiveRecord::Migration[5.1]
-  def change
-    remove_column :listings, :avatar_file_name, :string
-    remove_column :listings, :avatar_content_type, :string
-    remove_column :listings, :avatar_file_size, :integer
-    remove_column :listings, :avatar_updated_at, :datetime
-  end
-end
