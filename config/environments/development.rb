6e2804efc8488e186ad303ae932cc8833da1d8f2
DanielRusinov <daniel.rusinov00@gmail.com>
2018-05-30 00:49:28 +0300

    heroku deployment

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,7 +1,7 @@
 Rails.application.configure do
 
 
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.default_url_options = { host: 'asterdeal.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
 
