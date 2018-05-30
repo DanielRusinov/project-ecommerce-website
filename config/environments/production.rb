a5dd772f4331d35699014bc35aee49ca0ce193ea
DanielRusinov <daniel.rusinov00@gmail.com>
2018-05-30 20:53:37 +0300

    heroku deployment - AWS S3 Implementation

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -15,6 +15,17 @@ Rails.application.configure do
   :enable_starttls_auto => true
   }
 
+  config.paperclip_defaults = {
+        storage: :s3,
+        s3_credentials: {
+          bucket: ENV['S3_BUCKET_NAME'],
+          access_key_id: ENV['AWS_ACCESS_KEY_ID'],
+          secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
+          s3_region: ENV['S3_REGION'],
+          s3_host_name: ENV['S3_HOST_NAME']
+        }
+  }
+
 
   Paperclip.options[:command_path] = "/usr/bin/"
   # Code is not reloaded between requests.
